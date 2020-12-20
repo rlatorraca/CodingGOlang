6fd298cdd81871fa248c72e11ef6337de7692454
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-12-20 08:29:00 -0400

    Added Chapter 26(01-02)

---
+++ 26-Documentacao/26_02-Doc-GoDoc.go
@@ -0,0 +1,9 @@
+package _6_Documentacao
+
+/*
+- Antes de escrever documentação, vamos ver como lê-la. Temos algumas possibilidades:
+    - golang.org → documentação da standard library
+    - godoc.org → documentação da standard library e outros
+    - go doc → comando para ler documentação na linha de comando
+    - godoc → idem acima, para pode-se servir a documentação local via http
+ */
